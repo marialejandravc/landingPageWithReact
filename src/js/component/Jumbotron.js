0836a008cd18aed03f4ff97856c4a536b041a619
Maria Alejandra <mariale102008@gmail.com>
2024-06-07 13:35:09 +0200

    add jumbotron js

---
+++ src/js/component/Jumbotron.js
@@ -0,0 +1,15 @@
+// src/component/Jumbotron.js
+import React from "react";
+
+const Jumbotron = () => {
+    return (
+        <div className="jumbotron text-center mb-5">
+            <h1 className="display-4">A Warm Welcome</h1>
+            <p className="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ullamcorper, neque molestie faucibus bibendum, tortor tellus laoreet tortor, eget accumsan enim metus eu est. Fusce sit amet tortor turpis. Sed a metus at massa ornare facilisis quis id erat.</p>
+            <a className="boton btn btn-primary btn-lg d-flex"href="#" role="button">Call to action!</a>
+        </div>
+     
+    );
+};
+
+export default Jumbotron;
